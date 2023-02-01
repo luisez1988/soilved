c2a2b6402a7642e73cbfa6373c4c688e7cead697
luisez1988 <luisez@vt.edu>
2023-01-31 21:26:29 -0500

    completed examples mod 2

--- content/Soilved.py
+++ content/Soilved.py
@@ -89,6 +89,191 @@ def GetSrinkageLimit(V_dry, Ws, gamma_w=9.81, Gs=2.67):
     return ((V_dry*gamma_w/Ws)-(1/Gs))*100 
 
 
+def GetUSCS(GF, SF, FF, Cu=1000, Cc=10000, is_organic=False,PI=0, LL=0):
+    #GF=        Gravel fraction in %
+    #SF=        Sand fraction in %
+    #FF=        Fines fraction in %
+    #Cu=        Coefficient of uniformity default is high meaning well graded
+    #Cc=        Coefficient of curvature default for well graded mat.
+    #PI=        Plasticity index in % default 0
+    #LL=        Liquid limit, default 0
+    #Is_organic Flag to indicate material is organic
+    prefix=""
+    sufix=""
+    if (FF>= 50): #material is clay, silt, or organic
+        if (LL<50): #low plasticity
+            if (is_organic): #material is organic
+                Group="OL"
+                Name="Low-plastic organic soil"
+            else: #material is not organic
+                PIA=0.73*(LL-20) #Plasticity index on the A-line                
+                if ((PI>7) and (PI>=PIA)):#CL
+                    Group="CL"
+                    prefix, sufix=GetDetailedName_fines(FF, SF, GF)
+                    if (prefix==""):
+                        Name="Lean clay %s" %sufix
+                    else:
+                        Name="%s lean clay %s" %(prefix, sufix)
+                elif((PI>=4) and (PI<=7) and (PI>=PIA)): #CL-ML
+                    Group="CL-ML"
+                    prefix, sufix=GetDetailedName_fines(FF, SF, GF)
+                    if (prefix==""):
+                        Name="Silty clay %s" %sufix
+                    else:
+                        Name="%s silty clay %s" %(prefix, sufix)
+                else: #ML
+                    Group="ML"
+                    prefix, sufix=GetDetailedName_fines(FF, SF, GF)
+                    if (prefix==""):
+                        Name="Silt %s" %sufix
+                    else:
+                        Name="%s silt %s" %(prefix, sufix)
+
+
+        else: #high plasticity
+            if (is_organic): #material is organic
+                Group="OH"
+                Name="High-plastic organic soil"
+            else: #material is not organic
+                PIA=0.73*(LL-20) #Plasticity index on the A-line                
+                if (PI>=PIA):#CH
+                    Group="CL"
+                    prefix, sufix=GetDetailedName_fines(FF, SF, GF)
+                    if (prefix==""):
+                        Name="Fat clay %s" %sufix
+                    else:
+                        Name="%s fat clay %s" %(prefix, sufix)
+                else: #MH
+                    Group="MH"
+                    prefix, sufix=GetDetailedName_fines(FF, SF, GF)
+                    if (prefix==""):
+                        Name="Elastic silt %s" %sufix
+                    else:
+                        Name="%s elastic silt %s" %(prefix, sufix)
+
+    else: #material is coarse (sand or gravel)
+        if (GF>=SF): #Gravel
+            if (FF<5):
+                if ((Cu>=4) and (Cc>=1) and (Cc<=3)): #GW
+                    Group="GW"
+                    if (SF<15):
+                        Name="Well-graded gravel"
+                    else:
+                        Name="Well-graded gravel with sand"
+                else: #GP
+                    Group="GP"
+                    if (SF<15):
+                        Name="Poorly-graded gravel"
+                    else:
+                        Name="Poorly-graded gravel with sand"
+            elif (FF>12):
+                Group, _, prefix=GetMinorFraction_coarse(PI, LL, "G")
+                if (SF>=15):
+                    Name="%s gravel with sand" %prefix
+                else:
+                    Name="%s gravel" %prefix
+            else:
+                if ((Cu>=4) and (Cc>=1) and (Cc<=3)): #GW
+                    Group="GW"
+                    Name="Well-graded gravel"
+                else: #GP
+                    Group="GP"
+                    Name="Poorly-graded gravel"
+                second_group, sufix, _=GetMinorFraction_coarse(PI, LL, "G")
+                Group= "%s-%s" %(Group, second_group)
+                Name= "%s %s" %(Name, sufix)
+                if (SF>=15):
+                    Name="%s and sand" %Name
+        else: #sand
+            if (FF<5):
+                if ((Cu>=4) and (Cc>=1) and (Cc<=3)): #SW
+                    Group="SW"
+                    if (GF<15):
+                        Name="Well-graded sand"
+                    else:
+                        Name="Well-graded sand with gravel"
+                else: #SP
+                    Group="SP"
+                    if (GF<15):
+                        Name="Poorly-graded sand"
+                    else:
+                        Name="Poorly-graded sand with gravel"
+            elif (FF>12):
+                Group, _, prefix=GetMinorFraction_coarse(PI, LL, "S")
+                if (GF>=15):
+                    Name="%s sand with gravel" %prefix
+                else:
+                    Name="%s sand" %prefix
+            else:
+                if ((Cu>=4) and (Cc>=1) and (Cc<=3)): #SW
+                    Group="SW"
+                    Name="Well-graded sand"
+                else: #sP
+                    Group="SP"
+                    Name="Poorly-graded sand"
+                second_group, sufix, _=GetMinorFraction_coarse(PI, LL, "S")
+                Group= "%s-%s" %(Group, second_group)
+                Name= "%s %s" %(Name, sufix)
+                if (GF>=15):
+                    Name="%s and gravel" %Name
+    return Group, Name
+
+def GetDetailedName_fines(FF, SF, GF):
+    Percent_coarser=100-FF #percent coarser
+    sufix=""
+    prefix=""
+    if (Percent_coarser<30):
+        if (Percent_coarser<15):
+            sufix=""
+        elif(SF>=GF):
+            sufix="with sand"
+        else:
+            sufix="with gravel"
+    else:
+        if (SF>GF):
+            if (GF<15):
+                prefix="Sandy"
+            else:
+                prefix="Sandy"
+                sufix="with gravel"
+        else:
+            if (SF<15):
+                prefix="Gravelly"
+            else:
+                prefix="Gravelly"
+                sufix="with sand"
+    return prefix, sufix
+
+def GetMinorFraction_coarse(PI, LL, FL):
+    PIA=0.73*(LL-20) #Plasticity index on the A-line 
+    sufix=""
+    prefix=""
+    if (LL<50): #low plasticity                      
+        if ((PI>7) and (PI>=PIA)):#C
+            Group="%sC" %FL
+            sufix="with clay"
+            prefix="Clayey"
+        elif((PI>=4) and (PI<=7) and (PI>=PIA)): #C-M
+            Group="%sC-%sM" %(FL, FL)
+            sufix="with silty clay"
+            prefix="Silty, clayey"
+        else: #M
+            Group="%sM" %FL
+            sufix="with silt"
+            prefix="Silty"
+    else: #high plasticity              
+        if (PI>=PIA):#C
+            Group="%sC" %FL
+            sufix="with clay"
+            prefix="Clayey"
+        else: #M
+            Group="%sM" %FL
+            sufix="with silt"
+            prefix="Silty"
+    return Group, sufix, prefix
+
+
+
 
 
 
